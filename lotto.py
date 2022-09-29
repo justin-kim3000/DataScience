0cce10ed3b8512f9004a5d18cbcfd3f50004243b
Justin_Kim <65004984+justin-kim3000@users.noreply.github.com>
2022-09-29 16:31:14 +0900

    add random result

--- lotto.py
+++ lotto.py
@@ -1,16 +1,33 @@
+import random
+
 numlist = []
+res = []
+
+# while 1:
+#     if len(numlist) == 6:
+#         break
+
+#     value = int(input())
+
+#     if 1 <= value <= 45:
+#         if value not in numlist:
+#             numlist.append(value)
+#         numlist = list(set(numlist))
+#         numlist.sort()
+#         print("입력한 숫자는 %s입니다." % numlist)
+#     else:
+#         print("1부터 45까지의 숫자만 넣어주세요.")
+# print(numlist)
+numlist = [1, 3, 6, 28, 34, 44]
+
 while 1:
-    if len(numlist) == 6:
+    if len(res) == 6:
         break
 
-    value = int(input())
+    randValue = random.randint(1, 45)
 
-    if 1 <= value <= 45:
-        if value not in numlist:
-            numlist.append(value)
-        numlist = list(set(numlist))
-        numlist.sort()
-        print("입력한 숫자는 %s입니다." % numlist)
-    else:
-        print("1부터 45까지의 숫자만 넣어주세요.")
-print(numlist)
+    if randValue not in res:
+        res.append(randValue)
+    res = list(set(res))
+    res.sort()
+print(res)
